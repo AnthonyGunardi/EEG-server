ef3d2a97b98a29c2e92ee60f2dc2d263bbc31f07
AnthonyGunardi <anthonygunardi@gmail.com>
2023-10-26 10:06:18 +0700

    disable existing user checking for IKF. Can undo this later

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,10 +7,10 @@ exports.register = async (req,res,next)=>{
           nama_ayah, nama_ibu, nama_smp, nama_sma, warna_favorit, nama_perusahaan, jabatan
         } = req.body
  
-        const user = await User.findOne({nama_lengkap, nama_ibu})
-        if (user) {
-            return res.status(401).json({message:"User already exist!"})
-        }
+        // const user = await User.findOne({nama_lengkap, nama_ibu})
+        // if (user) {
+        //     return res.status(401).json({message:"User already exist!"})
+        // }
         const newUser = await User.create({ nama_lengkap, nama_panggilan, tanggal_lahir, kota_kelahiran, jenis_kelamin,
           nama_ayah, nama_ibu, nama_smp, nama_sma, warna_favorit, nama_perusahaan, jabatan
         })
