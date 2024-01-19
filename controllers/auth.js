e3f984ac7b94142b4ab0c3c10b103773ac39963d
AnthonyGunardi <anthonygunardi@gmail.com>
2024-01-19 15:48:25 +0700

    edit user schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ exports.register = async (req,res,next)=>{
     try {
         const {
           nama_lengkap, nama_panggilan, tanggal_lahir, kota_kelahiran, jenis_kelamin,
-          nama_ayah, nama_ibu, nama_smp, nama_sma, warna_favorit, nama_perusahaan, jabatan
+          nama_ayah, nama_ibu, nama_smp, nama_sma, warna_favorit, nama_perusahaan, jabatan, event_id
         } = req.body
  
         // const user = await User.findOne({nama_lengkap, nama_ibu})
@@ -12,7 +12,7 @@ exports.register = async (req,res,next)=>{
         //     return res.status(401).json({message:"User already exist!"})
         // }
         const newUser = await User.create({ nama_lengkap, nama_panggilan, tanggal_lahir, kota_kelahiran, jenis_kelamin,
-          nama_ayah, nama_ibu, nama_smp, nama_sma, warna_favorit, nama_perusahaan, jabatan
+          nama_ayah, nama_ibu, nama_smp, nama_sma, warna_favorit, nama_perusahaan, jabatan, event_id
         })
         return res.status(201).json(newUser)
     } catch (err) {
