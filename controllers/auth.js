1efbc4f4ff4cc31942a1ab66ba51272f1cf58cd4
AnthonyGunardi <anthonygunardi@gmail.com>
2024-02-05 15:53:21 +0700

    modify register method on auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ exports.register = async (req,res,next)=>{
     try {
         const {
           fullname, nickname, birthday, hometown, gender,
-          father, mother, junior_highschool, senior_highschool, favorite_color, userEvents, status
+          father, mother, junior_highschool, senior_highschool, favorite_color, userEvents, status, pauli_test, wcst_test, digitspan_test, validation_test
         } = req.body
  
         const user = await User.findOne({fullname, mother})
@@ -12,7 +12,7 @@ exports.register = async (req,res,next)=>{
             return res.status(401).json({message:"User already exist!"})
         }
         const newUser = await User.create({ fullname, nickname, birthday, hometown, gender,
-          father, mother, junior_highschool, senior_highschool, favorite_color, userEvents, status
+          father, mother, junior_highschool, senior_highschool, favorite_color, userEvents, status, pauli_test, wcst_test, digitspan_test, validation_test
         })
         return res.status(201).json(newUser)
     } catch (err) {
